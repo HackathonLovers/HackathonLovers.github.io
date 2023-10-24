e74f03d4ba587e1e0c2642a8ed2249ee237dc480
AngelVil92 <angelvil92@gmail.com>
2023-10-24 23:45:30 +0200

    añadido el jurado

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "611b95468871a25c70ff";
+/******/ 	var hotCurrentHash = "1ff1b4cb16bda17df864";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
